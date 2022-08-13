afaba57700c7d13309eeecb282e08b38ed649006
Oscar Salcedo <josueht15@gmail.com>
2022-08-13 13:53:41 -0500

    Initial Commit

---
+++ SpiritualMeter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpiritualMeter
+//
+//  Created by Oscar Salcedo Lopez on 13/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpiritualMeter/SpiritualMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiritualMeterApp.swift
+//  SpiritualMeter
+//
+//  Created by Oscar Salcedo Lopez on 13/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SpiritualMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
